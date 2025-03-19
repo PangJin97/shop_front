99faaafe73b55830170615e6e7f86b7b7b06909d
PangJin97 <kindred5795@naver.com>
2025-03-19 15:10:37 +0900

    frontEnd

--- src/components/practice/UploadTest.jsx
+++ src/components/practice/UploadTest.jsx
@@ -17,29 +17,38 @@ const UploadTest = () => {
   // 먼저 첨부파일 input 태그에서 선택한 파일을 저장할 변수
   const [firstFile, setFirstFile] = useState(null);
 
-  //자바로 데이터를 전달할 때 문자뿐만 아니라 파일 데이터도 가져간다는 것을 설정
+  //일반적으로 자바로 데이터를 전달할때는 문자로 전달되는데
+
+  //자바로 데이터를 전달할 때 문자뿐만 아니라 파일 데이터도 가져간다는 것을 설정한다.
+
   //post() 메서드의 세번째 매개변수로 fileConfig를 전달(이거해야 파일 첨부됨 => 약속된 문법)
   const fileConfig = { headers: { "Content-Type": "multipart/form-data" } };
 
-
-  //다음으로 form 데이터 객체 생성
-  //첨부파일 데이터를 자바로 전달하기 위해서는
-  //FormData() 객체를 사용
-  //=> 첨부파일, input태그 등의 모든 데이터를 자바로
-  //가져갈 수 있는 객체
-  const form = new FormData();
-  form.append("name", "hong");
-  form.append("age", 20);
-  form.append("firstFile", firstFile);
-
   const sendFile = () => {
-    axios.post(
-    "/api/test/upload1"
-    ,form
-    ,fileConfig
-  )
-    .then()
-    .catch();
+    //다음으로 form 데이터 객체 생성
+    //첨부파일 데이터를 자바로 전달하기 위해서는
+    //FormData() 객체를 사용
+    //=> 첨부파일, input태그 등의 모든 데이터를 자바로
+    //가져갈 수 있는 객체이다
+    const form = new FormData();
+ 
+    form.append("bookName", "hong");
+    form.append("bookPrice", 20);
+    //2개는 form객체 테스트용
+
+    //form 객체는 숫자 문자도 담을 수있고
+    form.append("firstFile", firstFile);
+    //파일도 담을 수 있다.
+    //그래서 자바로 전달하기 위해 form객체를 사용한다. 
+   
+    axios
+      .post(
+        "/api/test/upload1", //url
+        form, // 전달할 데이터
+        fileConfig //설정할 내용
+      )
+      .then()
+      .catch();
   };
 
   return (
@@ -58,9 +67,15 @@ const UploadTest = () => {
           //파일을 선택할때 마다 선택한 파일을 firstFile에 저장한다.
           setFirstFile(e.target.files[0]);
         }}
+
         //multiple //=> 이 속성을 사용하면 한번에 여러 파일 선택 가능
       />
-      <button type="button" onClick={(e) => {}}>
+      <button
+        type="button"
+        onClick={(e) => {
+          sendFile();
+        }}
+      >
         파일전송1
       </button>
     </div>
